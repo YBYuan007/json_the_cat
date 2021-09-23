b8f0bb9d3c3631d750ceb666b25f8d2bdcfe1ec3
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-23 23:35:30 +0000

    used eslinit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,16 @@
 const request = require("request");
 
 const fetchBreedDescription = function(breedName, callback) {
-// moving the main request logic into a function named fetchBreedDescription.
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, desc) => {
-    // error = `we did not find ${breedName}`; 
+    // error = `we did not find ${breedName}`;
 
-  if (Object.keys(JSON.parse(desc.body)).length === 0) {
-    callback(JSON.parse(error), null);
-  } else {
-  callback(null,JSON.parse(desc.body)[0]["description"])};
+    if (Object.keys(JSON.parse(desc.body)).length === 0) {
+      callback(JSON.parse(error), null);
+    } else {
+      callback(null,JSON.parse(desc.body)[0]["description"]);
+    }
   
-})};
+  });
+};
 
-module.exports = {fetchBreedDescription}; 
+module.exports = {fetchBreedDescription};
