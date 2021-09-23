07b71bc0f04650ea8ac1c994dcb21ffbd0a016e7
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-23 23:30:45 +0000

    refactored and added Mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,15 @@
 const request = require("request");
-const userRequest = process.argv[2];
 
+const fetchBreedDescription = function(breedName, callback) {
+// moving the main request logic into a function named fetchBreedDescription.
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, desc) => {
+    // error = `we did not find ${breedName}`; 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${userRequest}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  // breed name is not found
-  if (Object.keys(data).length === 0) {
-    console.log(`Ooops, we did not find ${userRequest} type of cat.`);
-  }
-  // request failed (error: null, body: message:"404")
-  if (Object.keys(data).includes("message")) {
-    console.log(`this is the message: ${body}`);
-  }
-  // found the result
-  console.log(data[0].description);
-});
+  if (Object.keys(JSON.parse(desc.body)).length === 0) {
+    callback(JSON.parse(error), null);
+  } else {
+  callback(null,JSON.parse(desc.body)[0]["description"])};
+  
+})};
 
+module.exports = {fetchBreedDescription}; 
